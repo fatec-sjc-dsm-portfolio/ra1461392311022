121e1f4815089f9595726422c7246b827a3bc842
leticiacarvalho04 <leticiahelena.oliveira@outlook.com>
2025-08-18 00:01:23 -0300

    atualizando homepage no package.json e alterando o roteador para createHashRouter no index.tsx

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -3,37 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import SobreMim from './component/sobremim/sobreMim';
 import Projetos from './component/projetos/projetos';
 import Contatos from './component/contatos/contatos';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
   },
   {
-    path:'/sobreMim',
+    path: '/sobreMim',
     element: <SobreMim />
   },
   {
-    path:'/projetos',
+    path: '/projetos',
     element: <Projetos />
   },
   {
-    path:'/contato',
+    path: '/contato',
     element: <Contatos />
   }
 ]);
 
-  const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 reportWebVitals();
